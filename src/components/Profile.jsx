55eb9b1e688dc3147dc1abf37249d81743a9cdaf
Daniel <dikey@protonmail.com>
2021-04-01 11:23:33 +0300

    add components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className="content">
+      <div>
+        <img src="https://cf.bstatic.com/images/hotel/max1024x768/206/206909491.jpg" alt="#"/>
+      </div>
+      <div> ava + description</div>
+      <div>
+      
+        my posts
+        <div>new post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2 1</div>
+          <div>post 3</div>
+        </div>
+      </div>
+    </div>
+      )
+}
+
+export default Profile
